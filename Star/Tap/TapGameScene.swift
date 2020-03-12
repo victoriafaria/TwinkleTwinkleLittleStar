3e40bb09154ec2def52f370a547e0b28663fc18b
Victoria Andressa Faria <victoriaamdf@gmail.com>
2020-03-12 00:02:28 -0300

    add tapGestures

---
+++ Star/Tap/TapGameScene.swift
@@ -0,0 +1,14 @@
+//
+//  TapGameScene.swift
+//  Star
+//
+//  Created by Victoria Andressa S. M. Faria on 11/03/20.
+//  Copyright © 2020 Victoria Andressa Faria. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class TapGameScene: SKScene {
+    
+}
